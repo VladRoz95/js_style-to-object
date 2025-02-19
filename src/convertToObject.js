bf1db856c5745b96bb74c97d21e0828e0067b709
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-02-19 19:21:18 +0200

    solution for task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,10 +7,7 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  const styleArray = sourceString
-    .split(';')
-    .map((style) => style.trim())
-    .filter(Boolean);
+  const styleArray = sourceString.split(';').map((style) => style.trim());
 
   const styleObject = styleArray.reduce((acc, style) => {
     const [property, value] = style.split(':').map((part) => part.trim());
